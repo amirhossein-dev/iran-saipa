396cf3c8fcd06b696a5eb64941f2045cbbb2727b
Amir Hossein saffarnia <dev.saffarnia@gmail.com>
2025-08-02 05:57:22 +0330

    working on scaffold and Quasar CLI

---
+++ demo-saipa-app/src/pages/IndexPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    />
+  </q-page> -->
+  <div class="m-8 p-4 bg-red-200 text-yellow-700 rounded-lg">تست کلاس‌های Tailwind</div>
+  <div class="bg-green-500 text-red p-4">تست Tailwind</div>
+</template>
+
+<script setup>
+//
+</script>
